f7c771e188796e1f739f3c3b9a9b6f70d43cf2b5
jycksyn <110639007+jycksyn@users.noreply.github.com>
2023-06-25 19:05:52 -0400

    delete migrations

--- prisma/migrations/20230618115134_user_121/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `auth_user_id` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "auth_user_id" STRING NOT NULL;
