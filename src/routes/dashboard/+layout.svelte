7f45e7d67dae479bbcfefc9a0b02e19f8ee38341
jycksyn <110639007+jycksyn@users.noreply.github.com>
2023-06-24 22:15:27 -0400

    create base dashboard ui

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { AppRail, AppRailAnchor, AppShell } from '@skeletonlabs/skeleton';
+	import { Calendar, User, UserGroup } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+    import { page } from '$app/stores';
+</script>
+
+<AppShell>
+
+<AppRail slot="sidebarLeft" class="h-screen">
+	<AppRailAnchor href={'/dashboard/session'} selected={$page.url.pathname === '/dashboard/session'}>
+		<svelte:fragment slot="lead">
+			<Icon class="h-6" src={Calendar} />
+		</svelte:fragment>
+		<span>Sessions</span>
+	</AppRailAnchor>
+	<AppRailAnchor>
+		<svelte:fragment slot="lead">
+			<Icon class="h-6" src={UserGroup} />
+		</svelte:fragment>
+		<span>Groups</span>
+	</AppRailAnchor>
+	<svelte:fragment slot="trail">
+		<AppRailAnchor>
+			<svelte:fragment slot="lead">
+				<Icon class="h-6" src={User} />
+			</svelte:fragment>
+		</AppRailAnchor>
+	</svelte:fragment>
+</AppRail>
+
+<div class="max-w-2xl m-auto">
+<main class="p-6">
+<slot />
+</main>
+</div>
+
+</AppShell>
---
+++ src/routes/dashboard/session/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Avatar, ProgressRadial } from '@skeletonlabs/skeleton';
+	import { Star } from '@steeze-ui/heroicons';
+	import { Icon } from '@steeze-ui/svelte-icon';
+</script>
+
+<h2 class="h2 font-semibold">Sessions</h2>
+
+<ul class="list m-8">
+	<li>
+		<div class="relative">
+            <Avatar class="absolute top-0 left-0" width="w-14" initials="A" />
+            <ProgressRadial class="absolute z-2 top-0 left-0" width="w-14" value={50}>
+            </ProgressRadial>
+        </div>
+        <div class="flex flex-col flex-auto">
+		<span class="font-bold">May 21 - June 21</span>
+		<span class="flex flex-row items-center"><Icon src={Star} class="fill-current h-4 w-4 mr-2 text-secondary-500-400-token" /> Chandrum RAs</span>
+    </div>
+		<span>...</span>
+	</li>
+</ul>
