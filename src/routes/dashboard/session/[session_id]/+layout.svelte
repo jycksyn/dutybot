b211f4dc1afacbd3d57a43759ee61bc0b903ec1f
jycksyn <110639007+jycksyn@users.noreply.github.com>
2023-08-06 19:09:24 -0400

    add navbar to session

--- src/routes/dashboard/session/[session_id]/+layout.svelte
+++ src/routes/dashboard/session/[session_id]/+layout.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
 	import BackButton from '$lib/components/BackButton.svelte';
 	import { prettyDate } from '$lib/dates';
+	import { TabAnchor } from '@skeletonlabs/skeleton';
+	import { TabGroup } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { UserGroup } from '@steeze-ui/heroicons';
 
 	export let data;
 
 	const { session } = data;
+
+	const SUBPAGES = [['Calendar', `/dashboard/session/${session.id}/calendar`]];
 </script>
 
 <div class="flex flex-col gap-4">
@@ -14,9 +21,22 @@
 	</header>
 
 	<section>
-		<h4 class="h4 mt-4">{session.group.name}</h4>
+		<h4 class="h4 mt-4">
+			<a href="/dashboard/group/{session.group_id}/sessions">
+				<Icon class="h-6 w-6 inline stroke-gray-500" src={UserGroup} />
+				{session.group.name}
+			</a>
+		</h4>
 		<h3 class="h3 mb-4 font-bold">{prettyDate(session.start)} - {prettyDate(session.end)}</h3>
 	</section>
+
+	<TabGroup justify="justify-center">
+		{#each SUBPAGES as [title, href]}
+			<TabAnchor {href} selected={$page.url.pathname.slice(0, href.length) === href}>
+				{title}
+			</TabAnchor>
+		{/each}
+	</TabGroup>
 
 	<slot />
 </div>
