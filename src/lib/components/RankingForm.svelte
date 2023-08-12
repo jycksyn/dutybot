5a5d44bf1173f3d9c904e2453935efe6fba65fc6
jycksyn <110639007+jycksyn@users.noreply.github.com>
2023-08-12 08:47:37 -0400

    add clear button

--- src/lib/components/RankingForm.svelte
+++ src/lib/components/RankingForm.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import type { ShiftWithType } from '$lib/dbtypes';
 	import type { shiftRankingSchema } from '$lib/forms';
-	import { ArrowDown, ArrowUp, RectangleGroup } from '@steeze-ui/heroicons';
+	import { ArrowDown, ArrowUp, RectangleGroup, XCircle } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import type { SuperForm } from 'sveltekit-superforms/client';
 	import Calendar from './calendar/Calendar.svelte';
 	import VotingDay from './calendar/VotingDay.svelte';
+	import { modalStore, type ModalSettings } from '@skeletonlabs/skeleton';
 
 	export let shifts: ShiftWithType[];
 	export let shiftRankingForm: SuperForm<typeof shiftRankingSchema>;
@@ -14,50 +15,65 @@
 
 	let rankingLastFirst = true;
 
-    let queue: string[][] = [];
+	let queue: string[][] = [];
 
-    $: {
-        form.update(($form) => {
-            const preferences: Record<string, number> = {};
-            let lastRank = rankingLastFirst ? shifts.length : 0;
-            const u = rankingLastFirst ? -1 : 1;
-            for (let group of rankingLastFirst ? [...queue].reverse() : queue) {
-                const ranking = (2 * lastRank + u * (group.length + 1)) / 2;
-                for (let shift_id of group) {
-                    preferences[shift_id] = ranking;
-                }
-                lastRank = lastRank + u * group.length;
-            }
-            return {...$form, preferences};
-        })
-    }
+	$: {
+		form.update(($form) => {
+			const preferences: Record<string, number> = {};
+			let lastRank = rankingLastFirst ? shifts.length : 0;
+			const u = rankingLastFirst ? -1 : 1;
+			for (let group of rankingLastFirst ? [...queue].reverse() : queue) {
+				const ranking = (2 * lastRank + u * (group.length + 1)) / 2;
+				for (let shift_id of group) {
+					preferences[shift_id] = ranking;
+				}
+				lastRank = lastRank + u * group.length;
+			}
+			return { ...$form, preferences };
+		});
+	}
 
-    let shouldGroup = false;
+	let shouldGroup = false;
 
-    const handleGroup = () => { 
-        if (shouldGroup) {
-            shouldGroup = false;
-        } else {
-            shouldGroup = true;
-            if (queue[queue.length - 1]?.length) {
-                queue = [...queue, []];
-            }
+	const handleGroup = () => {
+		if (shouldGroup) {
+			shouldGroup = false;
+		} else {
+			shouldGroup = true;
+			if (queue[queue.length - 1]?.length) {
+				queue = [...queue, []];
+			}
+		}
+	};
+
+	const confirmClear: ModalSettings = {
+		type: 'confirm',
+		// Data
+		title: 'Confirm Clear',
+		body: 'Are you sure you want to clear your ranked preferences?',
+		response: (r: boolean) => {
+            queue = [];
+            $form.preferences = {};
         }
-    }
+	};
+
+	const handleClear = () => modalStore.trigger(confirmClear);
 
 	const handleSelect = ({ detail: { shift_id } }: VotingDay['$$events_def']['select']) => {
-        if ($form.preferences[shift_id] == undefined) {
-            if (shouldGroup) {
-                const group = [...(queue[queue.length - 1] ?? []), shift_id];
-                if (rankingLastFirst) return queue = [group, ...queue.slice(0, -1)];
-                return queue = [...queue.slice(0, -1), group];
-            }
-            if (rankingLastFirst) return queue = [[shift_id], ...queue];
-            return queue = [...queue, [shift_id]];
-        }
-        console.log(shouldGroup, $form.preferences[shift_id], "hi")
-        queue = queue.map(group => group.filter(id => id != shift_id)).filter((group) => group.length);
-    }
+		if ($form.preferences[shift_id] == undefined) {
+			if (shouldGroup) {
+				const group = [...(queue[queue.length - 1] ?? []), shift_id];
+				if (rankingLastFirst) return (queue = [group, ...queue.slice(0, -1)]);
+				return (queue = [...queue.slice(0, -1), group]);
+			}
+			if (rankingLastFirst) return (queue = [[shift_id], ...queue]);
+			return (queue = [...queue, [shift_id]]);
+		}
+		console.log(shouldGroup, $form.preferences[shift_id], 'hi');
+		queue = queue
+			.map((group) => group.filter((id) => id != shift_id))
+			.filter((group) => group.length);
+	};
 
 	$: canChangeOrdering = [0, shifts.length].includes(Object.keys($form.preferences).length);
 </script>
@@ -84,19 +100,31 @@
 			total={shifts.length}
 			on:select={handleSelect}
 			preferences={$form.preferences}
-            group={shouldGroup ? queue[queue.length - 1] : undefined}
+			group={shouldGroup ? queue[queue.length - 1] : undefined}
 			{date}
 			shifts={dayShifts}
 		/>
 	</Calendar>
 
-	<button on:click={handleGroup} type="button" class="btn" class:variant-soft-primary={!shouldGroup} class:variant-soft-tertiary={shouldGroup}>
+	<button
+		on:click={handleGroup}
+		type="button"
+		class="btn"
+		class:variant-soft-primary={!shouldGroup}
+		class:variant-soft-secondary={shouldGroup}
+	>
 		<span class="flex flex-row justify-center items-center">
-		{#if shouldGroup}
-			<Icon class="h-4 mr-2" src={RectangleGroup} /> Finish Grouping
-		{:else}
-			<Icon class="h-4 mr-2" src={RectangleGroup} /> Group Equal Preferences
-		{/if}
-        </span>
+			{#if shouldGroup}
+				<Icon class="h-4 mr-2" src={RectangleGroup} /> Finish Grouping
+			{:else}
+				<Icon class="h-4 mr-2" src={RectangleGroup} /> Group Equal Preferences
+			{/if}
+		</span>
+	</button>
+
+	<button on:click={handleClear} type="button" class="btn variant-soft-tertiary">
+		<span class="flex flex-row justify-center items-center">
+			<Icon class="h-4 mr-2" src={XCircle} /> Clear Ranking
+		</span>
 	</button>
 </form>
